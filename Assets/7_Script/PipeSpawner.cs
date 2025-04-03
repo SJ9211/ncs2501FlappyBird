b8e230b84a4350600cbfece736d50c19e745d793
SJ9211 <jsj921127@naver.com>
2025-04-03 15:45:27 +0900

    RedPipe

--- Assets/7_Script/PipeSpawner.cs
+++ Assets/7_Script/PipeSpawner.cs
@@ -7,6 +7,7 @@ public class PipeSpawner : MonoBehaviour
     [SerializeField] float maxTime = 1.5f;          // 몇초 마다 생성할건지
     [SerializeField] float heightRange = 0.5f;      // 생성위치 y의 랜덤 범위
     [SerializeField] GameObject pipePrefab;         // 파이프의 Prefab 연결
+    [SerializeField] GameObject redPipePrefab;      // 빨간 파이프 프레팝 연결
     float timer;
  
     void Update()
@@ -27,6 +28,8 @@ public class PipeSpawner : MonoBehaviour
     {
         // 랜덤으로 y값을 정해서 생설될 파이프의 위치 정하기
         Vector3 spawnPos = transform.position + new Vector3(0, Random.Range(-heightRange, heightRange));
+        // 랜덤으로 녹색, 빨간색 파이프 선택
+        GameObject pipePf = (Random.Range(0, 100) > 10) ? pipePrefab : redPipePrefab;
         // Instantiate 로 생성, 생성된 객체는 pipe라는 GameObject에 할당
         GameObject pipe = Instantiate(pipePrefab, spawnPos, Quaternion.identity);
         // 5초 뒤에 pipe 객체 파괴
