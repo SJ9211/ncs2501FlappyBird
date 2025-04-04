44f34cbee4ef2de74e3c4e3323eb61bd4ff7affd
SJ9211 <jsj921127@naver.com>
2025-04-04 15:31:47 +0900

    State. Audio

--- Assets/7_Script/GameManager.cs
+++ Assets/7_Script/GameManager.cs
@@ -1,23 +1,117 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using Unity.Android.Types;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    // 게임 상태를 저장할 enum
+    public enum State
+    {
+        TITLE,      // 0 : 0
+        READY,      // 1 : 1
+        PLAY,       // 2 : 2
+        GAMEOVER,   // 3 : 3
+        BESTSCORE   // 4 : 4
+    }
     public static GameManager Instance;
 
-    [SerializeField] GameObject gameOverUI;
+    [SerializeField] SpriteRenderer background;
+    [SerializeField] Animator floorAnim;
+    [SerializeField] AudioClip acReady;
+    [SerializeField] AudioClip acHit;
+    [SerializeField] GameObject restartButton;
+    [SerializeField] GameObject[] stateUI;
+    [SerializeField] Sprite[] bgSprite;
+
+    State gameState; // 게임상태를 저장할 변수
+    new AudioSource audio;  // 컴퍼넌트에 오디오가 있어서 new 를 붙여준다.
+    public State GameState => gameState;
 
     void Awake()
     {
         if (Instance == null) Instance = this;
     }
 
+    void Start()
+    {
+        // AudioSource 연결
+        audio = GetComponent<AudioSource>();
+        // 정상적인 게임의 시간이 흐르게 만드는것
+        Time.timeScale = 1.0f;
+        // 게임의 시작은 Title에서
+        GameTitle();
+    }
+
+    public void PlayAudio(AudioClip clip)
+    {
+        // 파라미터로 넘어온 clip을 한번 플레이시킨다
+        audio.PlayOneShot(clip);
+    }
+
+    void ChangeState(State value)
+    {
+        gameState = value;
+        // stateUI에 있는 모든 UI를 끈다
+        foreach (var item in stateUI)
+        {
+            item.SetActive(false);
+        }
+
+        // State값을 공통으로 사용하므로, 미리 int 값으로 변환
+        int temp = (int)gameState;
+        // 해당하는 Background sprite 연결
+        background.sprite = bgSprite[temp % 2];
+        // 해당하는 stateUI를 켠다
+        stateUI[temp].SetActive(true);
+    }
+
+    public void GameTitle()
+    {
+        ChangeState(State.TITLE);
+    }
+
+    public void GameReady()
+    {
+        ChangeState(State.READY);
+        // Ready Sound
+        PlayAudio(acReady);
+    }
+
+    public void GamePlay()
+    {
+        ChangeState(State.PLAY);
+    }
+
     public void GameOver()
     {
-        gameOverUI.SetActive(true);
+        ChangeState(State.GAMEOVER);
+        // 피격 사운드
+        PlayAudio(acHit);
+        // floor 애니메이션을 멈춤다
+        floorAnim.enabled = false;
+        // restart 버튼을 일단 꺼둔다
+        restartButton.SetActive(false);
+        // 코루틴을 사용해서 잠시 시간을 지연시킨다
+        StartCoroutine(StopTimer());
+    }
+
+    IEnumerator StopTimer()
+    {
+        // 2초 기다렸다 다음 로직 실행
+        yield return new WaitForSeconds(2f);
+        // 게임 시간을 멈춘다.
+        Time.timeScale = 0f;
+        // restart 버튼 나오게
+        restartButton.SetActive(true);
+
+    }
+
+    public void GameBestScore()
+    {
+        ChangeState(State.BESTSCORE);
     }
 
     public void RestartGame()
--- Assets/7_Script/PipeSpawner.cs
+++ Assets/7_Script/PipeSpawner.cs
@@ -7,13 +7,16 @@ public class PipeSpawner : MonoBehaviour
     [SerializeField] float maxTime = 1.5f;          // 몇초 마다 생성할건지
     [SerializeField] float heightRange = 0.5f;      // 생성위치 y의 랜덤 범위
     [SerializeField] GameObject pipePrefab;         // 파이프의 Prefab 연결
-    [SerializeField] GameObject redPipePrefab;      // 빨간 파이프 프레팝 연결
+    [SerializeField] GameObject RedPipePrefab;      // 빨간 파이프 프레팝 연결
     float timer;
- 
+
     void Update()
     {
+        // 게임상태가 PLAY일때만 파이프를 생성
+        if (GameManager.Instance.GameState != GameManager.State.PLAY) return;
+
         // timer 가 maxTime 를 넘으면
-        if ( timer > maxTime)
+        if (timer > maxTime)
         {
             // pipe를 만드는 함수 호출
             SpawnPipe();
@@ -29,10 +32,10 @@ public class PipeSpawner : MonoBehaviour
         // 랜덤으로 y값을 정해서 생설될 파이프의 위치 정하기
         Vector3 spawnPos = transform.position + new Vector3(0, Random.Range(-heightRange, heightRange));
         // 랜덤으로 녹색, 빨간색 파이프 선택
-        GameObject pipePf = (Random.Range(0, 100) > 10) ? pipePrefab : redPipePrefab;
+        GameObject pipePf = (Random.Range(0, 100) > 10) ? pipePrefab : RedPipePrefab;
         // Instantiate 로 생성, 생성된 객체는 pipe라는 GameObject에 할당
         GameObject pipe = Instantiate(pipePrefab, spawnPos, Quaternion.identity);
         // 5초 뒤에 pipe 객체 파괴
-        Destroy(pipe, 5);
+        Destroy(pipe, 5f);
     }
 }
